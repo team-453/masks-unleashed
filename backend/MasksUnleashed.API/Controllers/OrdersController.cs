d95d5b2934d135541bb2cf462f24fc2216212f06
Esteban Luchsinger <eluchsingerm@hotmail.com>
2020-04-04 17:47:13 +0200

    Added an API to get all the recycling orders

---
+++ backend/MasksUnleashed.API/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MasksUnleashed.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasksUnleashed.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        /// <summary>
+        /// Returns all recycling orders.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("recycling")]
+        public Task<List<RecyclingOrder>> GetAllRecyclingOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
