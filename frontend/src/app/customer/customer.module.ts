3f509523ab63b94fdc5e0cf0eeb6b12f50be0a79
Esteban Luchsinger <eluchsingerm@hotmail.com>
2020-04-04 14:53:11 +0200

    Include the contact formular into the dashboard

--- frontend/src/app/customer/customer.module.ts
+++ frontend/src/app/customer/customer.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
+import { UserDetailsFormComponent } from './user-details-form/user-details-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserDashboardComponent, UserDetailsFormComponent],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class CustomerModule {
