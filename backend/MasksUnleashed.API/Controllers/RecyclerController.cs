39a83a97304927800379869a3844d60d1200963f
Esteban Luchsinger <eluchsingerm@hotmail.com>
2020-04-04 17:41:57 +0200

    Added orders API for the recyclers

--- backend/MasksUnleashed.API/Controllers/RecyclerController.cs
+++ backend/MasksUnleashed.API/Controllers/RecyclerController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using MasksUnleashed.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MasksUnleashed.API.Controllers
@@ -13,7 +15,7 @@ namespace MasksUnleashed.API.Controllers
         /// </summary>
         /// <param name="recyclerId">ID of the Recycler (GUID)</param>
         /// <returns>Returns the amount of masks the recycler is able to recycle.</returns>
-        [HttpGet("capacity/{recyclerId}")]
+        [HttpGet("{recyclerId}/capacity")]
         public Task<int> GetCapacity([FromRoute]Guid recyclerId)
         {
             throw new NotImplementedException();
@@ -25,8 +27,31 @@ namespace MasksUnleashed.API.Controllers
         /// <param name="recyclerId">ID of the Recycler (GUID)</param>
         /// <param name="amountOfMasks">Amount of infected masks</param>
         /// <returns>Sets the amount of masks the recycler is able to recycle.</returns>
-        [HttpPost("capacity/{recyclerId}")]
-        public Task<int> SetCapacity(Guid recyclerId, int amountOfMasks)
+        [HttpPost("{recyclerId}/capacity")]
+        public Task<int> SetCapacity([FromRoute]Guid recyclerId, int amountOfMasks)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all the orders of the recycler.
+        /// </summary>
+        /// <param name="recyclerId">The GUID ID of the recycler.</param>
+        /// <returns>Returns all orders of the recycler</returns>
+        [HttpGet("{recyclerId}/orders")]
+        public Task<List<RecyclingOrder>> GetOrders([FromRoute]Guid recyclerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a new order to the recycler.
+        /// </summary>
+        /// <param name="recyclerId">The GUID ID of the recycler.</param>
+        /// <param name="recyclingOrder">The new recycling order</param>
+        /// <returns>Returns all orders of the recycler</returns>
+        [HttpPut("{recyclerId}/orders")]
+        public Task AddOrder([FromRoute]Guid recyclerId, RecyclingOrder recyclingOrder)
         {
             throw new NotImplementedException();
         }
---
+++ backend/MasksUnleashed.Core/Models/RecyclingOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MasksUnleashed.Core.Models
+{
+    public class RecyclingOrder
+    {
+        public Guid Id { get; set; }
+        public Guid Recycler { get; set; }
+        public int MaskRecyclingCapacity { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<AcceptedRecyclingOrder> AcceptedOrders { get; set; } = new List<AcceptedRecyclingOrder>();
+
+        public class AcceptedRecyclingOrder
+        {
+            public Guid CollectorId { get; set; }
+            public string CollectorName { get; set; }
+            public int MasksToDeliver { get; set; }
+        }
+    }
+}
