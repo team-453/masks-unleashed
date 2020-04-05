aa0649736665a1b036916411fb66b5cdb2e48fd0
Esteban Luchsinger <eluchsingerm@hotmail.com>
2020-04-05 14:12:57 +0200

    Make Matteo happy

--- frontend/src/app/customer/collection-point-map/collection-point-map.component.ts
+++ frontend/src/app/customer/collection-point-map/collection-point-map.component.ts
@@ -5,27 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './collection-point-map.component.html',
   styleUrls: ['./collection-point-map.component.scss']
 })
-export class CollectionPointMapComponent implements OnInit {
+export class CollectionPointMapComponent {
   latitude = 47.387084;
   longitude = 8.514754;
   zoom = 14;
 
   markers: Marker[] = [
-    {label: 'Collection Point 2', latitude: 47.387084, longitude: 8.514754},
-    {label: 'Collection Point Hardbrücke', latitude: 47.388231, longitude: 8.519773}
+    {
+      label: 'Collection Point Zürich Hauptbahnhof',
+      openingTimes: '10:00 - 12:00 13:00 - 17:00',
+      reviews: '⭐⭐⭐⭐⭐',
+
+      latitude: 47.387084, longitude: 8.514754
+    },
+    {
+      label: 'Collection Point Hardbrücke',
+      openingTimes: '08:00 - 12:00 13:00 - 20:00',
+      reviews: '⭐⭐⭐⭐',
+      latitude: 47.388231, longitude: 8.519773
+    }
   ];
 
-  constructor() {
+
+  onMouseOver(infoWindow, $event: MouseEvent) {
+    infoWindow.open();
   }
 
-  ngOnInit(): void {
 
+  onMouseOut(infoWindow, $event: MouseEvent) {
+    infoWindow.close();
   }
-
 }
 
 interface Marker {
   label: string;
+  openingTimes: string;
   latitude: number;
   longitude: number;
+  reviews: string;
 }
